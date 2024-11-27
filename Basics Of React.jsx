24585c98b1d656dd4d3e15172aa83df087d2734f
Huzaifa Khan <huzaifazaifi25@gmail.com>
2024-11-27 12:03:12 +0500

    Basics Of React.jsx

---
+++ Basics Of React.jsx
@@ -0,0 +1,26 @@
+//-------I. Basics of React--------//
+
+//=====What is React?=====//
+
+//---A JavaScript library for building user interfaces (UIs).
+//---Created and maintained by Facebook.
+//---Uses a component-based architecture to make code reusable and maintainable.
+
+//======Key Features of React:=====//
+
+//----It is a Component-Based Architecture.
+//----It Works On Mechanism Of Declarative Approach.
+//----It uses a virtual DOM to improve performance.
+//----It uses a syntax extension called JSX, which allows you to write HTML-like code directly in JavaScript.
+//----React enforces a one-way data flow, meaning data flows from a parent component to its child components.
+
+//======Why Use React?=========//
+
+//-----Efficiency: React handles UI updates efficiently, even in applications with complex, dynamic user interfaces.
+//-----Reusability: Components can be reused across different parts of an application or even in other projects.
+//-----Scalability: Ideal for applications that need to scale up, with thousands of components.
+//-----Ecosystem: React has a rich ecosystem of tools, libraries, and a large community for support.
+
+//=====Summary=====//
+
+//-----In summary, React is a powerful tool for building modern, responsive, and maintainable web applications. Its component-based and declarative nature simplifies UI development while enhancing performance and scalability.---//
